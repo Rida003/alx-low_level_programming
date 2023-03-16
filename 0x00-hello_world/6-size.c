ca09ba3d1cc7ea80820c2bfc2cf5715ae42a386a
Rida003 <redaessahi@gmail.com>
2023-03-16 08:13:15 -0700

    Task fiEXD

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Description: using sizeof to print the size of various types.
- *
  * Return: Always 0 (Succesful)
 */
 
@@ -18,7 +15,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 
 }
