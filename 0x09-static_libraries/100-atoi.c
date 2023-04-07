ea179638c74ea44d10e56f38e0a3e0dc4ef3cde6
Rida003 <redaessahi@gmail.com>
2023-04-07 09:29:25 -0700

    Task done

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,12 +1,20 @@
-#include "main.h"
-
 /**
- * _atoi - the firt whole int number in a string.
- * @s: string to convert to int
- *
- * Return: INT .
- */
-int _atoi(char *s __attribute__((unused)))
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
+int _atoi(char *s)
 {
-        return (0);
+int i = 1;
+unsigned int num = 0;
+do {
+if (*s == '-')
+i *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *i);
 }
