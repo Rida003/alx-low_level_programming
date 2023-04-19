e736a6f43435e7b2990c704af12bcc37512a8cf1
Rida003 <redaessahi@gmail.com>
2023-04-19 20:49:50 +0000

    Task done

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 
-
 /**
  * op_add - calculates the sum of two integers
  * @a: first integer
@@ -8,6 +7,7 @@
  *
  * Return: sum of a and b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -20,6 +20,7 @@ int op_add(int a, int b)
  *
  * Return: difference of a and b
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
@@ -32,6 +33,7 @@ int op_sub(int a, int b)
  *
  * Return: product of a and b
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
@@ -44,6 +46,7 @@ int op_mul(int a, int b)
  *
  * Return: result of the division of a and b
  */
+
 int op_div(int a, int b)
 {
 	return (a / b);
@@ -56,7 +59,13 @@ int op_div(int a, int b)
  *
  * Return: remainder of a divided b
  */
+
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
-#ifndef FUNCTION_PIONTER_H
-#define FUNCTION_PIONTER_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_PIONTERS_H
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+
 #endif
