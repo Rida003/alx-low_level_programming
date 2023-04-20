cf77225d99a9fc039d9df70609840ed29928161c
Rida003 <redaessahi@gmail.com>
2023-04-20 13:35:34 +0000

    Task done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,23 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+
+
+typedef struct print_ops
+{
+	char *op;
+	void (*f)(va_list);
+} p_op;
+
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void pr_int(va_list);
+void pr_char(va_list);
+void pr_float(va_list);
+void pr_str(va_list);
 
 #endif
