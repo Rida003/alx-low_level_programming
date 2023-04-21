1f0fcb9971791375843a6bba1a126792c6877f74
Rida003 <redaessahi@gmail.com>
2023-04-21 11:49:59 +0000

    Task done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,12 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-
+/**
+ * struct print_ops - struct use to match the operator
+ * with correlating function
+ * @op: string represention of the operator.
+ * @f: function pointer for the operation to be used.
+ */
 typedef struct print_ops
 {
 	char *op;
