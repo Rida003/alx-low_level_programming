5361cee087cd4566ccab4dfd662aa47396261b3a
Rida003 <redaessahi@gmail.com>
2023-04-30 22:58:45 +0100

    Task done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-  * print listint - print the int value in each element of a list.
-  * @h: pointer to the head of a list.
-  *
-  * Return: Number of element in a list.
-*/
+ * print_listint - print the int value in each element of a list.
+ * @h: pointer to the head of a list.
+ *
+ * Return: Number of elements in a list.
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t n_nodes = 0;
