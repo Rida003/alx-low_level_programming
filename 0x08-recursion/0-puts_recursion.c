34a464b1ef2ca710accc848c1317593deca874a2
Rida003 <redaessahi@gmail.com>
2023-10-26 22:14:58 +0200

    Task fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  * @s:string
  * Return:void
 */
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
 	if (*s)
 	{
@@ -12,7 +12,7 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-		{
+	{
 			_putchar('\n');
 		}
 }
