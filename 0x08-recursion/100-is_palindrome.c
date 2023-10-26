4e020205c094d3e5a49b0f6154d4d82096599c20
Rida003 <redaessahi@gmail.com>
2023-10-26 22:36:12 +0200

    Fixed

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,13 +8,13 @@ int count_string(char *s);
  *   * @s: string to check.
  *    *
  *     * Return: 1 if a string is a palindrome and 0 if not.
- *      */
+*/
 int is_palindrome(char *s)
 {
 		int count;
 
 			count = 0;
-				if (!(*s))
+			if (!(*s))
 							return (1);
 
 					count = count_string(s);
@@ -28,15 +28,15 @@ int is_palindrome(char *s)
  *     * @b_i: back index.
  *      *
  *       * Return: 1 if a string is a palindrome and 0 if not.
- *        */
+*/
 int find_pal(char *s, int f_i, int b_i)
 {
-		/*printf("%c - %c : %d - %d\n", s[f_i], s[b_i], f_i, b_i); */
+/*printf("%c - %c : %d - %d\n", s[f_i], s[b_i], f_i, b_i); */
 		if (s[f_i] != s[b_i])
 					return (0);
-			if (s[f_i] == s[b_i] && f_i == b_i)
+					if (s[f_i] == s[b_i] && f_i == b_i)
 						return (1);
-				else if (s[f_i] == s[b_i] && f_i > b_i)
+						else if (s[f_i] == s[b_i] && f_i > b_i)
 							return (1);
 					else
 								return (find_pal(s, f_i + 1, b_i - 1));
@@ -47,10 +47,10 @@ int find_pal(char *s, int f_i, int b_i)
  *   * @s: string to count.
  *    *
  *     * Return: Int value of the length.
- *      */
+*/
 int count_string(char *s)
 {
-		if (*s)
+	if (*s)
 					return (count_string((s + 1)) + 1);
 			return (0);
 }
