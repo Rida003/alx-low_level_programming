353b25ae1a1d010d49b28a8acf4ce11f911c217b
Rida003 <redaessahi@gmail.com>
2023-10-26 22:44:11 +0200

    Fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,52 +1,29 @@
 #include "main.h"
 
-int sqrt_rec_odd(int start, int n);
 /**
- *  * _sqrt_recursion - find the square root of a given number.
- *   * @n: number to find the square root of.
- *    *
- *     * Return: the sqrt as a Int.
- *      */
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
+*/
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
 int _sqrt_recursion(int n)
 {
-		int sqrt;
-
-			if (n < 0)
-						return (-1);
-				else if (n <= 1 && n >= 0)
-							return (n);
-
-					if (!(n % 2))
-							{
-										if (n / 2 == 2 || n == 2)
-														return (2);
-												sqrt = _sqrt_recursion(n / 2);
-														if (n / sqrt == sqrt)
-																		return (sqrt);
-																else if (sqrt > 1)
-																				return (sqrt * 2);
-																		return (-1);
-																			}
-						else
-								{
-											return (sqrt_rec_odd(n, n - 2));
-												}
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
-
-/**
- *  * sqrt_rec_odd - helper to deal with odd numbers and primes.
- *   * @start: the odd or prime number to check for sqrt.
- *    * @n: number to find the square root of.
- *     *
- *      * Return: the sqrt as a Int.
- *       */
-int sqrt_rec_odd(int start, int n)
-{
-		if (n < 0)
-					return (-1);
-			else if (start == n * n)
-						return (n);
-				else
-							return (sqrt_rec_odd(start, n - 2));
-}
-
