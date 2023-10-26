aeae1eda6a03a5c2eba0426bd7587419edc1a1f3
Rida003 <redaessahi@gmail.com>
2023-10-26 22:38:21 +0200

    Fixed

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-
-int find_pal(char *s, int f_i, int b_i);
-int count_string(char *s);
 /**
- *  * is_palindrome - checks if string is a palindrom
- *   * @s: string to check.
- *    *
- *     * Return: 1 if a string is a palindrome and 0 if not.
-*/
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * pal_checker - check if s is palindrome.
+ * @s: string base address.
+ * @i: left index.
+ * @j: rigth index.
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int pal_checker(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - check if s is palindrome
+ * @s: base address for string.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
 int is_palindrome(char *s)
 {
-		int count;
-
-			count = 0;
-			if (!(*s))
-							return (1);
-
-					count = count_string(s);
-						return (find_pal(s, 0, count - 1));
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
-
-/**
- *  * find_pal - helper for is_palindrom
- *   * @s: string to check
- *    * @f_i: front index.
- *     * @b_i: back index.
- *      *
- *       * Return: 1 if a string is a palindrome and 0 if not.
-*/
-int find_pal(char *s, int f_i, int b_i)
-{
-/*printf("%c - %c : %d - %d\n", s[f_i], s[b_i], f_i, b_i); */
-		if (s[f_i] != s[b_i])
-					return (0);
-					if (s[f_i] == s[b_i] && f_i == b_i)
-						return (1);
-						else if (s[f_i] == s[b_i] && f_i > b_i)
-							return (1);
-					else
-								return (find_pal(s, f_i + 1, b_i - 1));
-}
-
-/**
- *  * count_string - count the string.
- *   * @s: string to count.
- *    *
- *     * Return: Int value of the length.
-*/
-int count_string(char *s)
-{
-	if (*s)
-					return (count_string((s + 1)) + 1);
-			return (0);
-}
-
