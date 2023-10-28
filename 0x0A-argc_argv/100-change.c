402a39e4a2a8275ca1b88bacb4105eb3b26de1a3
Rida003 <redaessahi@gmail.com>
2023-10-28 12:35:55 +0200

    Done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int give_change(int money);
-
 /**
- * main - prints the minimum number of coins to make change
+ * main - prints the min number of coins to make change
  * for an amount of money
- * @argc: number of arguments.
- * @argv: amount of money given in singles.
- *
- * Return: 0 (sucesss) , 1 (Failure)
-*/
-int main(int argc, char *argv[])
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
 {
-	int money, change;
-
-	money = change = 0;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
@@ -23,55 +21,31 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	money = atoi(argv[1]);
-	change = give_change(money);
+	total = strtol(argv[1], &p, 10);
+	count = 0;
 
-	printf("%d\n", change);
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
 	return (0);
 }
-
-/**
- * give_change - find the minimum number of coins to make change
- * for an amount of money
- * @money: amount of money given in singles.
- *
- * Return: min amount of coins. (INT)
-*/
-int give_change(int money)
-{
-	int one, two, five, ten, tf, sum;
-
-	sum = one = two = five = ten = tf = 0;
-
-	if (money <= 0)
-	{
-		return (0);
-	}
-
-	if (money >= 25)
-	{
-		tf = money / 25;
-		money -= tf * 25;
-	}
-
-	if (money < 25 && money >= 10)
-	{
-		ten = money / 10;
-		money -= ten * 10;
-	}
-
-	if (money < 10 && money >= 5)
-	{
-		five = money / 5;
-		money -= five * 5;
-	}
-	if (money < 5 && money >= 2)
-		two = money / 2;
-		money -= two * 2;
-	if (money < 2)
-		one = money;
-
-	sum = tf + ten + five + two + one;
-	return (sum);
-}
-
